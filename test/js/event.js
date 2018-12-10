d634fb26009198723ab3a30ec03e60f4ba7c528f
Ganesan Murugesan <rmdganesan@gmail.com>
2018-12-10 10:13:15 +0300

    event js update

--- test/js/event.js
+++ test/js/event.js
@@ -1,201 +1,41 @@
 var event = {
-    '1980': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '1981': ["Iran Iraq war continues", "OPEC agreement for $32/bbl", "Exxon withdraws from Libya", "Windfall profits tax reduced."],
-    '1982': ["Iran Iraq war continues", "Damascus closes Iraqs export pipeline of 400,000bbl/d ", "U.S. boycotts Libyan crude.", "Windfall profits tax reduced."],
-    '1983': ["Iran Iraq war continues", "NYMEX  starts trading in crude oil futures", "U.S. boycotts Libyan crude.", "Windfall profits tax reduced."],
-    '1984': ["Iran Iraq war continues", "Norway and Britain cut oil prices", "Oil spot market falls ", "PEC cuts production to 16 Mbbl/d"],
-    '1985': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '1986': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '1987': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '1988': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '1989': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '1990': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '1991': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '1991': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '1992': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '1993': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '1994': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '1995': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '1996': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '1999': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '1999': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '2000': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '2001': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '2002': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '2003': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '2004': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '2005': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '2006': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '2007': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '2008': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '2009': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '2010': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '2011': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '2012': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '2013': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '2014': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '2015': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '2016': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '2017': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-    '2018': ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""],
-
-
+    '1980': ["Saudi Aramco reverts to state ownership", "Supply shock", "Collapse of OPEC's  pricing structure", "Iran Iraq war"],
+    '1981': ["Iran Iraq war", "OPEC agreement for $32/bbl", "Exxon withdraws from Libya", "Windfall profits tax reduced"],
+    '1982': ["Iran Iraq war", "Damascus closes Iraqs export pipeline of 400,000bbl/d", "U.S. boycotts Libyan crude", "Windfall profits tax reduced"],
+    '1983': ["Iran Iraq war", "NYMEX  starts trading in crude oil futures", "U.S. boycotts Libyan crude", "Windfall profits tax reduced"],
+    '1984': ["Iran Iraq war", "Norway and Britain cut oil prices", "Oil spot market falls", "PEC cuts production to 16 Mbbl/d"],
+    '1985': ["Iran Iraq war", "OPEC cuts gap between light and heavy crudes", "OPEC output falls to 20-year low of 13.7 Mbbl/d", "Saudi links  prices to spot market"],
+    '1986': ["Iran Iraq war", "Saudi Arabia ramps up production", "Saudi output rises from 2 Mbbl/d to 5Mbbl/d", "OPEC output  hits 18 Mbbl/d"],
+    '1987': ["Iran Iraq war", "Saudi Arabia ramps up production", "Saudi output rises from 2 Mbbl/d to 5Mbbl/d", "OPEC output  hits 18 Mbbl/d"],
+    '1988': ["Iran Iraq war ends", "Output increasse to pre-war level", "OPEC/Non-OPEC meeting failure", "production agreement at GCC  meet"],
+    '1989': ["Exxon Valdez major oil spill", "Oil prices up after news of oil shortage", "OPEC raises  production ceiling to 19.5 Mbbl/d", "production agreement at GCC  meet"],
+    '1990': ["Gulf war begins", "Oil prices plunge", "Production from Kuwait stops", "Major earthquake in Iran"],
+    '1991': [" Gulf war ends", "Surplus  inventory hits 80-90 MMB.", "OPEC announces production cut to 22.3 Mbbl/d |", "Collapse of Soviet Union"],
+    '1992': ["Kuwait starts oil production", "OPEC production reaches high of  25.25 Mbbl/d", "U.S.A., Mexico, and Canada sign the NAFTA", "NYMEX Futures trades  $22 /bbl"],
+    '1993': ["Iraq accepts U.N inspection", "OPEC overproduction", "U.S.A., Mexico, and Canada sign the NAFTA", "NYMEX Futures trades  $22 /bbl"],
+    '1994': ["Nigerian oil worker strike", "OPEC overproduction", "U.S.A., Mexico, and Canada sign the NAFTA", "NYMEX Futures trades  $22 /bbl"],
+    '1995': ["Saudi and Norway meet to stabilize price", "Exxon to develop Russia's Sakhalin field", "Saudi Aramco contracts Parsons, for Shaybah", "US embargo on Iran's oil"],
+    '1996': ["Exxon starts work on Russia's Sakhalin", "US starts sale of Strategic Petroleum Reserve", "Saudi Aramco contracts Parsons, for Shaybah", "US embargo on Iran's oil"],
+    '1997': ["Asian Financial crisis", "Qatar  develops  world's largest LNG plant", "Kazakhstan starts exporting oil", "OPEC raises ceiling to 27.5mbbl/d"],
+    '1998': ["South Korea cuts production by 20%.", "OPEC cuts oil by 1.245mddl/d", "Exxon mergs with Mobil", "BP buys Amoco"],
+    '1999': ["Asian markets Recover", "OPEC and non-OPEC cut output by 2.104 mbbl/d", "CPC  builds a 932-mile pipeline", "Hybrid cars hits road"],
+    '2000': [" Investment falls in oil sectore", "OPEC increase in oil production by 1.452 mbbl/d", "Saudi Arabia  opens up to foreign investors", "NYMEX crude oil at 10 yrs high"],
+    '2001': ["Major economic instability after 9/11.", "Oil workers strike in Venezuela", "OPEC cut output by 4%", "NYMEX crude oil October delivery falls"],
+    '2002': ["OPEC cuts oil by 1.5mbbl/d", "Oil workers strike in Venezuela", "Conoco and Phillips Petrolium merge", "Russia cuts output by .15mbbl/d"],
+    '2003': ["OPEC raise output to 24.5mbbl/d", "NYMEX future at $37.83", "OPEC holds current output", "Russia's Yukos and Sibneft merge"],
+    '2004': ["OPEC raise output to 24.5mbbl/d", "NYMEX future at $37.84", "OPEC holds current output", "Russia's Yukos and Sibneft merge"],
+    '2005': ["Asian middle class rise boost oil demand", "Decline in Saudi oil production", "Russia's Yukos and Sibneft merge", "Saudi stock market crash"],
+    '2006': ["Asian middle class rise boost oil demand", "Decline in Saudi oil production", "Russia's Yukos and Sibneft merge", "Saudi stock market crash"],
+    '2007': ["Global finansial crisis", "Oil sector is hit hard", "Collapse of large financial institute", "Stock Market drops world wide"],
+    '2008': ["Oil price hits high $147 then bottom at $32", "Consumer wealth decline trillions of $", "Saudi stock recovers", "Stock Market drops world wide"],
+    '2009': ["Oil price hits high $147 then bottom at $33", "Consumer wealth decline trillions of $", "Saudi stock recovers", "Electric cars hit road"],
+    '2010': ["Arab Spring. Libian civil war", "Revolution in Tunisia", " European economy slows down", "Deepwater Horizon oil spill"],
+    '2011': ["Instability in Egypt, Libya, Yemen, Bahrain", "February oil prices at $95/bbl", "US dollar weakens", "Worries over the U.S. economy decreases"],
+    '2012': ["Planned closure of refinaries in US", "Iran ends sales to Britain and France", "Greek economic crisis.", "Higher Third World demand"],
+    '2013': ["Feb EU stocks up", "Federal Reserve may  end stimulus", "Trouble in Egypt", "Higher Third World demand."],
+    '2014': ["Oil demand falls", "Japans economy doing well", "Libiya and North Sea oil up", "U.S. shale  oil production increased"],
+    '2015': ["Oil prices fall steadily", "U.S. shale oil production decreases", "U.S. inventories highest since 1982", "Civil war in Yemen"],
+    '2016': ["Pipeline outages in Nigeria and Iraq", " UK votes to leave EU", "Russia planning to join OPEC", "OPEC countries to limit output"],
+    '2017': ["Feb U.S. inventories at record high.", "Production increase in Nigeria and Libya", "US $ moves up", "Pipeline hit in Libya and the North Sea"],
+    '2018': ["EU–Japan Economic Partnership Agreement signed", "Elections in Iraq", "CO2 emission increases to record high", "US exports oil for the first time"]
 };
-
-
-[
-    ["1"],
-    ["Saudi Aramco reverts to state ownership ", "Supply shock ", "Collapse of OPEC's  pricing structure", ""]
-]
-[
-    ["13"],
-    ["Iran Iraq war continues", "OPEC agreement for $32/bbl", "Exxon withdraws from Libya", "Windfall profits tax reduced."]
-]
-[
-    ["25"],
-    ["Iran Iraq war continues", "Damascus closes Iraqs export pipeline of 400,000bbl/d ", "U.S. boycotts Libyan crude.", "Windfall profits tax reduced."]
-]
-[
-    ["37"],
-    ["Iran Iraq war continues", "NYMEX  starts trading in crude oil futures", "U.S. boycotts Libyan crude.", "Windfall profits tax reduced."]
-]
-[
-    ["49"],
-    ["Iran Iraq war continues", "Norway and Britain cut oil prices", "Oil spot market falls ", "PEC cuts production to 16 Mbbl/d"]
-]
-[
-    ["61"],
-    ["Iran Iraq war continues", "OPEC cuts gap between light and heavy crudes ", "OPEC output falls to 20-year low of 13.7 Mbbl/d ", "Saudi links  prices to spot market. "]
-]
-[
-    ["73"],
-    ["Iran Iraq war continues", "Saudi Arabia ramps up production", "Saudi output rises from 2 Mbbl/d to 5Mbbl/d ", "OPEC output  hits 18 Mbbl/d "]
-]
-[
-    ["85"],
-    ["Iran Iraq war continues", "Saudi Arabia ramps up production", "Saudi output rises from 2 Mbbl/d to 5Mbbl/d ", "OPEC output  hits 18 Mbbl/d "]
-]
-[
-    ["97"],
-    ["Iran Iraq war ends", "Output increasse to pre-war level ", "OPEC/Non-OPEC meeting failure.", "production agreement at GCC  meet"]
-]
-[
-    ["109"],
-    ["Exxon Valdez major oil spill", "Oil prices up after news of oil shortage.", "OPEC raises  production ceiling to 19.5 Mbbl/d ", "production agreement at GCC  meet"]
-]
-[
-    ["121"],
-    ["Gulf war begins", "Oil prices plunge", "Production from Kuwait stops", "Major earthquake in Iran"]
-]
-[
-    ["133"],
-    [" Gulf war ends ", "Surplus  inventory hits 80-90 MMB.", "OPEC announces production cut to 22.3 Mbbl/d |", "Collapse of Soviet Union."]
-]
-[
-    ["145"],
-    ["Kuwait starts oil production ", "OPEC production reaches high of  25.25 Mbbl/d", "U.S.A., Mexico, and Canada sign the NAFTA.", "NYMEX Futures trades  $22 /bbl"]
-]
-[
-    ["157"],
-    ["Iraq accepts U.N inspection", "OPEC overproduction", "U.S.A., Mexico, and Canada sign the NAFTA.", "NYMEX Futures trades  $22 /bbl"]
-]
-[
-    ["169"],
-    ["Nigerian oil worker strike", "OPEC overproduction", "U.S.A., Mexico, and Canada sign the NAFTA.", "NYMEX Futures trades  $22 /bbl"]
-]
-[
-    ["181"],
-    ["Saudi and Norway meet to stabilize price", "Exxon to develop Russia's Sakhalin field", "Saudi Aramco contracts Parsons, for Shaybah ", "US embargo on Iran's oil "]
-]
-[
-    ["193"],
-    ["Exxon starts work on Russia's Sakhalin", "US starts sale of Strategic Petroleum Reserve", "Saudi Aramco contracts Parsons, for Shaybah ", "US embargo on Iran's oil "]
-]
-[
-    ["205"],
-    ["Asian Financial crisis", "Qatar  develops  world's largest LNG plant.", "Kazakhstan starts exporting oil", "OPEC raises ceiling to 27.5mbbl/d "]
-]
-[
-    ["217"],
-    ["South Korea cuts production by 20%.", "OPEC cuts oil by 1.245mddl/d", "Exxon mergs with Mobil", "BP buys Amoco"]
-]
-[
-    ["229"],
-    ["Asian markets Recover", "OPEC and non-OPEC cut output by 2.104 mbbl/d", "CPC  builds a 932-mile pipeline ", "Hybrid cars hits road"]
-]
-[
-    ["241"],
-    [" Investment falls in oil sectore ", "OPEC increase in oil production by 1.452 mbbl/d ", "Saudi Arabia  opens up to foreign investors.", "NYMEX crude oil at 10 yrs high."]
-]
-[
-    ["253"],
-    ["Major economic instability after 9/11.", "Oil workers strike in Venezuela", "OPEC cut output by 4% ", "NYMEX crude oil October delivery falls "]
-]
-[
-    ["265"],
-    ["OPEC cuts oil by 1.5mbbl/d", "Oil workers strike in Venezuela", "Conoco and Phillips Petrolium merge.", "Russia cuts output by .15mbbl/d"]
-]
-[
-    ["277"],
-    ["OPEC raise output to 24.5mbbl/d ", "NYMEX future at $37.83", "OPEC holds current output.", "Russia's Yukos and Sibneft merge."]
-]
-[
-    ["289"],
-    ["OPEC raise output to 24.5mbbl/d ", "NYMEX future at $37.84", "OPEC holds current output.", "Russia's Yukos and Sibneft merge."]
-]
-[
-    ["301"],
-    ["Asian middle class rise boost oil demand. ", "Decline in Saudi oil production", "Russia's Yukos and Sibneft merge.", "Saudi stock market crash"]
-]
-[
-    ["313"],
-    ["Asian middle class rise boost oil demand. ", "Decline in Saudi oil production", "Russia's Yukos and Sibneft merge.", "Saudi stock market crash"]
-]
-[
-    ["325"],
-    ["Global finansial crisis", "Oil sector is hit hard.", "Collapse of large financial institute.", "Stock Market drops world wide. "]
-]
-[
-    ["337"],
-    ["Oil price hits high $147 then bottom at $32", "Consumer wealth decline trillions of $", "Saudi stock recovers", "Stock Market drops world wide. "]
-]
-[
-    ["349"],
-    ["Oil price hits high $147 then bottom at $33", "Consumer wealth decline trillions of $", "Saudi stock recovers", "Electric cars hit road"]
-]
-[
-    ["361"],
-    ["Arab Spring. Libian civil war ", "Revolution in Tunisia.  ", " European economy slows down ", "Deepwater Horizon oil spill "]
-]
-[
-    ["373"],
-    ["Instability in Egypt, Libya, Yemen, Bahrain", "February oil prices at $95/bbl", "US dollar weakens.", "Worries over the U.S. economy decreases"]
-]
-[
-    ["385"],
-    ["Planned closure of refinaries in US", "Iran ends sales to Britain and France ", "Greek economic crisis.", "Higher Third World demand."]
-]
-[
-    ["397"],
-    ["Feb EU stocks up", "Federal Reserve may  end stimulus ", "Trouble in Egypt.", "Higher Third World demand."]
-]
-[
-    ["409"],
-    ["Oil demand falls", "Japans economy doing well", "Libiya and North Sea oil up", "U.S. shale  oil production increased"]
-]
-[
-    ["421"],
-    ["Oil prices fall steadily", "U.S. shale oil production decreases", "U.S. inventories highest since 1982", "Civil war in Yemen"]
-]
-[
-    ["433"],
-    ["Pipeline outages in Nigeria and Iraq", " UK votes to leave EU ", "Russia planning to join OPEC", "OPEC countries to limit output"]
-]
-[
-    ["445"],
-    ["Feb U.S. inventories at record high.", "Production increase in Nigeria and Libya", "US $ moves up ", "Pipeline hit in Libya and the North Sea"]
-]
-[
-    ["457"],
-    ["EU–Japan Economic Partnership Agreement is signed", "Elections in Iraq", "CO2 emission increases to record high", "US exports more oil than it imports for the first time"]
-]
