9c50a2763837a11d23e179734c2c94127735c3ab
Ganesan Murugesan <rmdganesan@gmail.com>
2018-12-11 12:32:34 +0300

    update the number format

--- test/js/bar-chart.js
+++ test/js/bar-chart.js
@@ -5,21 +5,16 @@ var dataAxis = ["Iraq", "Canada", "UAE", "Venezuela", "Mexico", "China", "Iran",
 var data = [2095, 2103, 2147, 2216, 2281, 2605, 3336, 8675, 9767, 11570];
 var yMax = 15500;
 var dataShadow = [];
-
 for (var i = 0; i < data.length; i++) {
     dataShadow.push(yMax);
 }
-
 require([
     'echarts' /*, 'map/js/china' */
 ], function(echarts) {
-
-
     var option = {
         title: {
-            text: '1980 January - Top 10 Production country',
+            text: 'Top 10 Nation (K) bbl / day - 1980 January ',
         },
-
         color: ['#17a2b8', '#006699', '#4cabce', '#e5323e'],
         tooltip: {
             trigger: 'axis',
@@ -32,7 +27,7 @@ require([
         },
         grid: {
             left: '3%',
-            right: '4%',
+            right: '10%',
             bottom: '3%',
             containLabel: true
         },
@@ -51,7 +46,6 @@ require([
             }
         },
         yAxis: {
-
             data: dataAxis,
             axisLabel: {
                 inside: true,
@@ -67,67 +61,57 @@ require([
             },
             z: 10
         },
-        series: [{ // For shadow
-                type: 'bar',
-                itemStyle: {
-                    normal: {
-                        color: 'rgba(0,0,0,0.05)'
-                    }
+        series: [{
+            type: 'bar',
+            itemStyle: {
+                normal: {
+                    color: new echarts.graphic.LinearGradient(
+                        0, 0, 0, 1, [{
+                                offset: 0,
+                                color: '#7cd6d2'
+                            },
+                            {
+                                offset: 0.5,
+                                color: '#61adaa'
+                            },
+                            {
+                                offset: 1,
+                                color: '#61adaa'
+                            }
+                        ]
+                    )
                 },
-                barGap: '-100%',
-                barCategoryGap: '40%',
-                data: dataShadow,
-                animation: false
+                emphasis: {
+                    color: new echarts.graphic.LinearGradient(
+                        0, 0, 0, 1, [{
+                                offset: 0,
+                                color: '#7cd6d2'
+                            },
+                            {
+                                offset: 0.7,
+                                color: '#7cd6d2'
+                            },
+                            {
+                                offset: 1,
+                                color: '#7cd6d2'
+                            }
+                        ]
+                    )
+                }
             },
-            {
-                type: 'bar',
-                itemStyle: {
-                    normal: {
-                        color: new echarts.graphic.LinearGradient(
-                            0, 0, 0, 1, [{
-                                    offset: 0,
-                                    color: '#7cd6d2'
-                                },
-                                {
-                                    offset: 0.5,
-                                    color: '#61adaa'
-                                },
-                                {
-                                    offset: 1,
-                                    color: '#61adaa'
-                                }
-                            ]
-                        )
+            label: {
+                normal: {
+                    show: true,
+                    position: 'right',
+                    formatter: function(value) {
+                        return value.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                            // return value.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
                     },
-                    emphasis: {
-                        color: new echarts.graphic.LinearGradient(
-                            0, 0, 0, 1, [{
-                                    offset: 0,
-                                    color: '#7cd6d2'
-                                },
-                                {
-                                    offset: 0.7,
-                                    color: '#7cd6d2'
-                                },
-                                {
-                                    offset: 1,
-                                    color: '#7cd6d2'
-                                }
-                            ]
-                        )
-                    }
-                },
-                label: {
-                    normal: {
-                        show: true,
-                        position: 'right'
-                    }
-                },
-                data: data
-            }
-        ]
+                }
+            },
+            data: data
+        }]
     };
-
     bar_chart = testHelper.create(echarts, 'bar-chart', {
         option: option,
         height: 300,
@@ -136,7 +120,6 @@ require([
 });
 
 function changeBarChart(yearIndex) {
-
     option_info = bar_chart.getOption();
     data = option_info.series[0].data;
     yMax = Math.max.apply(null, production[yearIndex]);
@@ -144,12 +127,12 @@ function changeBarChart(yearIndex) {
     for (var i = 0; i < data.length; i++) {
         dataShadow.push(yMax);
     }
+
     option_info.yAxis[0].data = country[yearIndex];
-    option_info.series[0].data = dataShadow;
-    option_info.series[1].data = production[yearIndex];
-    option_info.title[0].text = month_year[yearIndex].year + ' ' + month_year[yearIndex].month + '  - Top 10 Production country';
+    //option_info.series[0].data = dataShadow;
+    option_info.series[0].data = production[yearIndex];
+    option_info.title[0].text = ' Top 10 Nation (K) bbl / day - ' + month_year[yearIndex].year + ' ' + month_year[yearIndex].month;
     // alert(country[yearIndex] + '  ' + production[yearIndex]);
     bar_chart.setOption(option_info, true);
-
     // alert(yearIndex);
 }
